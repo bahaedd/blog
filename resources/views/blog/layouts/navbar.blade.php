f37f108abc598f41d943170d8715c31096fbdd9e
bahaeddine <sihassi.bahaeddine@gmail.com>
2022-12-30 21:21:19 +0100

    someChanges

--- resources/views/blog/layouts/navbar.blade.php
+++ resources/views/blog/layouts/navbar.blade.php
@@ -25,15 +25,23 @@
         <div class="hidden justify-between sm:justify-center items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-3">
             <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <li>
-                    <a href="{{ route('home') }}" class="text-sm font-medium text-gray-900 hover:text-green-600 dark:text-gray-300 dark:hover:text-green-500" aria-current="page">HOME</a>
+                    <a href="{{ route('home') }}" class="text-sm font-medium text-gray-900 hover:text-green-600 dark:text-gray-300 dark:hover:text-green-500 uppercase" aria-current="page">Blog</a>
                 </li>
-                @foreach ($categories as $category)
                 <li>
-                    <a href="/blog/category/{{ $category->slug }}" class="text-sm font-medium text-gray-900 hover:text-green-600 dark:text-gray-300 dark:hover:text-green-500 uppercase">{{ $category->name }}</a>
+                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-900  md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-300 dark:hover:text-white dark:focus:text-white dark:border-gray-700  uppercase">Tutorials <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
+                <!-- Dropdown menu -->
+                <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                        @foreach ($categories as $category)
+                          <li>
+                            <a href="/blog/category/{{ $category->slug }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $category->name }}</a>
+                          </li>
+                        @endforeach
+                    </ul>
+                </div>
                 </li>
-                @endforeach
                 <li>
-                    <a href="{{ route('portfolio') }}" class="text-sm font-medium text-gray-900 hover:text-green-600 dark:text-gray-300 dark:hover:text-green-500">PORTFOLIO</a>
+                    <a href="{{ route('portfolio') }}" class="text-sm font-medium text-gray-900 hover:text-green-600 dark:text-gray-300 dark:hover:text-green-500 uppercase">about</a>
                 </li>
             </ul>
         </div>
