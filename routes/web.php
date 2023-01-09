a0d1c27e50a13264f5431d702c9988934be7853c
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-01-09 22:44:33 +0100

    refactoring003

--- routes/web.php
+++ routes/web.php
@@ -103,3 +103,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
