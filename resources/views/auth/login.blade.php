6a62e8f48d695e4e2a289bd12d25a8a47da13b37
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-01-11 23:24:55 +0100

    refactoring004

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,9 @@
 @extends('blog/main')
+@section('title', 'Login')
 @section('content')
 <section class="bg-gray-50 dark:bg-gray-800 mt-12">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a href="{{ route('home') }}" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             <img class="w-8 h-8 mr-2" src="{{Voyager::image( setting('site.logo'))}}" alt="logo">
             AlienDev
         </a>
