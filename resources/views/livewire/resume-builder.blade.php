d10f53756df03e45e167e449abd7359fc6019fb5
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-01-27 20:35:17 +0100

    ResumeBuilder4.2

--- resources/views/livewire/resume-builder.blade.php
+++ resources/views/livewire/resume-builder.blade.php
@@ -27,11 +27,11 @@
                                     <div>
                                         @if($personal_informations)
                                         <div class="flex items-center justify-center w-full">
-                                            <img src="{{URL('/storage/profiles/'.$personal_informations->image)}}" class="h-36 rounded-full sm:h-56" alt="bahaeddine" width="230" height="160">
+                                            <img src="{{URL('/storage/profiles/'.$personal_informations->image)}}" class="h-auto max-w-full rounded-lg" alt="{{ $personal_informations->name }}" width="230" height="160">
                                         </div>
                                         @else
                                         <div class="flex items-center justify-center w-full">
-                                            <img src="{{URL('/images/guest.jpg')}}" class="h-36 rounded-full sm:h-56" alt="bahaeddine" width="230" height="160">
+                                            <img src="{{URL('/images/guest.jpg')}}" class="h-auto max-w-full rounded-lg" alt="{{ Auth::user()->name }}" width="230" height="160">
                                         </div>
                                         @endif
                                     </div>
@@ -414,8 +414,8 @@
                                     <div class="md:w-1/3 w-full">
                                         <header>
                                             @if($personal_informations)
-                                            <img src="{{URL('/storage/profiles/'.$personal_informations->image)}}" class="h-36 rounded-full sm:h-56" alt="{{ $personal_informations->name }}" width="230" height="160">
-                                            <div class="text-green dark:text-white mt-4">
+                                            <img src="{{URL('/storage/profiles/'.$personal_informations->image)}}" class="h-auto max-w-full rounded-lg" alt="{{ $personal_informations->name }}" width="230" height="160">
+                                            <div class="text-green dark:text-white mt-6">
                                                 <a href="https://aliendev.com" class="hover:underline flex items-center mt-1">
                                                     <ion-icon name="person" class="mr-2"></ion-icon> {{ $personal_informations->name }}
                                                 </a>
