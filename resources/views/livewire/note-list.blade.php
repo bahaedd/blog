1282ff34298257f89b1c84c5cb3002f6633bdafd
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-04 12:34:57 +0100

    setup02

--- resources/views/livewire/note-list.blade.php
+++ resources/views/livewire/note-list.blade.php
@@ -19,15 +19,25 @@
                                     </div>
                                 </div>
                                 <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
-                                    <div class="py-1 px-6 border-b border-gray-300">
-                                        <ion-icon name="attch" size="large"></ion-icon>
+                                    <div class="py-3 px-6 border-b border-gray-300">
+                                        <ion-icon name="attach" size="large"></ion-icon>
                                     </div>
                                     <div class="p-6">
                                         <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
-                                        <p class="text-gray-700 text-base mb-4">
+                                        <p class="text-gray-700 text-base mb-6">
                                             With supporting text below as a natural lead-in to additional content.
                                         </p>
-                                        <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                                    </div>
+                                </div>
+                                <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
+                                    <div class="py-3 px-6 border-b border-gray-300">
+                                        <ion-icon name="attach" size="large"></ion-icon>
+                                    </div>
+                                    <div class="p-6">
+                                        <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                        <p class="text-gray-700 text-base mb-6">
+                                            With supporting text below as a natural lead-in to additional content.
+                                        </p>
                                     </div>
                                 </div>
                             </div>
