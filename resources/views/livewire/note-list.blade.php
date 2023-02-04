f63e993820457273ec98bde60989aa995f41b815
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-04 03:38:27 +0100

    setup02

--- resources/views/livewire/note-list.blade.php
+++ resources/views/livewire/note-list.blade.php
@@ -1,62 +1,26 @@
 <div>
     <div>
         <div class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 mx-24">
-            <!-- Add Note --->
-            <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 mb-4 dark:bg-gray-800 mx-auto">
-                <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mx-3 p-6">
-                    <form class="space-y-6" action="#">
-                        <h5 class="text-xl font-medium text-gray-900 dark:text-white text-center">Add note</h5>
-                        <div>
-                            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                            <input wire:model="state.title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white @error('title') border-red-500 @enderror" placeholder="Your Habit title..." required>
-                        </div>
-                        <div>
-                            <label for="underline_select" class="sr-only">Category</label>
-                            <select id="underline_select" wire:model="state.category" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-                                <option value="">Select a category</option>
-                                <option value="personal">Personal</option>
-                                <option value="work">Work</option>
-                                <option value="others">Other</option>
-                            </select>
-                        </div>
-                        {{-- @if ($updateMode)
-                        <button type="submit" wire:click.prevent="update" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
-                        @else
-                        <button type="submit" wire:click.prevent="store" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
-                        @endif --}}
-                    </form>
-                    @if ($errors->any())
-                    <div class="flex p-4 mt-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
-                        <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
-                        </svg>
-                        <span class="sr-only">Info</span>
-                        <div>
-                            @foreach ($errors->all() as $error)
-                            <span class="font-medium">{{ $error }}</span>
-                            @endforeach
-                        </div>
-                    </div>
-                    @endif
-                </div>
-                <div class="flex justify-items-center mt-12 ml-3">
-                    <a href="{{ route('personalpack') }}" class="text-green-700 mt-4 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
-                        <ion-icon name="chevron-back-outline"></ion-icon> Back to PersonalPack
-                    </a>
-                </div>
-            </div>
             <!-- Notes list --->
             <div class="w-full sm:w-full md:w-full lg:w-full xl:w-full mb-4 dark:bg-gray-800 mx-auto">
                 <div class="md:flex lg:flex xl:flex justify-between bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mx-3 p-6">
                     <div class="w-full overflow-x-auto relative shadow-md sm:rounded-lg m-3 mr-3">
-                        <div class="text-center py-4 bg-white dark:bg-gray-800">
-                            <h5 class="text-xl font-medium text-gray-900 dark:text-white text-center">Notes</h5>
-                        </div>
-                        <div class="w-full bg-gray-800 border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                            {{-- <div class="flex justify-center">
-                                <div class="block rounded-lg shadow-lg bg-yellow-300 max-w-sm text-center m-12">
+                        <div class="w-full bg-gray-800 border shadow-md dark:bg-gray-800 dark:border-gray-700">
+                            <div class="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+                                <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
                                     <div class="py-3 px-6 border-b border-gray-300">
-                                        Featured
+                                        <ion-icon name="attach" size="large"></ion-icon>
+                                    </div>
+                                    <div class="p-6">
+                                        <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                        <p class="text-gray-700 text-base mb-6">
+                                            With supporting text below as a natural lead-in to additional content.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
+                                    <div class="py-1 px-6 border-b border-gray-300">
+                                        <ion-icon name="attch" size="large"></ion-icon>
                                     </div>
                                     <div class="p-6">
                                         <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
@@ -65,33 +29,8 @@
                                         </p>
                                         <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
                                     </div>
-                                    <div class="py-3 px-6 border-t border-gray-300 text-gray-600">
-                                        2 days ago
-                                    </div>
                                 </div>
-                            </div> --}}
-                            <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-                                        <div class="max-w-md mx-auto">
-                                            <div>
-                                                <h1 class="text-2xl font-semibold">Login Form with Floating Labels</h1>
-                                            </div>
-                                            <div class="divide-y divide-gray-200">
-                                                <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                                                    <div class="relative">
-                                                        <input autocomplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-                                                        <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
-                                                    </div>
-                                                    <div class="relative">
-                                                        <input autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-                                                        <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-                                                    </div>
-                                                    <div class="relative">
-                                                        <button class="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                            </div>
                         </div>
                     </div>
                 </div>
