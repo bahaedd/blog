b422b363891be2ed2d0614dd03c86215d0596991
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-05 12:35:03 +0100

    MultiNotes02

--- resources/views/livewire/note-list.blade.php
+++ resources/views/livewire/note-list.blade.php
@@ -6,40 +6,179 @@
                 <div class="md:flex lg:flex xl:flex justify-between bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mx-3 p-6">
                     <div class="w-full overflow-x-auto relative shadow-md sm:rounded-lg m-3 mr-3">
                         <div class="w-full bg-gray-800 border shadow-md dark:bg-gray-800 dark:border-gray-700">
-                            <div class="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
-                                <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
-                                    <div class="py-3 px-6 border-b border-gray-300">
-                                        <ion-icon name="attach" size="large"></ion-icon>
-                                    </div>
-                                    <div class="p-6">
-                                        <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
-                                        <p class="text-gray-700 text-base mb-6">
-                                            With supporting text below as a natural lead-in to additional content.
-                                        </p>
+                            <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+                                <ul class=" w-full flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+                                    <li class="mr-2" role="presentation">
+                                        <button class="inline-block p-4 border-b-2 rounded-t-lg" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+                                    </li>
+                                    <li class="mr-2" role="presentation">
+                                        <button class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Dashboard</button>
+                                    </li>
+                                    <li class="mr-2" role="presentation">
+                                        <button class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Settings</button>
+                                    </li>
+                                    <li role="presentation">
+                                        <button class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab" aria-controls="contacts" aria-selected="false">Contacts</button>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div id="myTabContent">
+                                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                    <div class="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+                                        <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-green-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-blue-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="block shadow-lg bg-green-300 max-w-sm text-center m-12">
-                                    <div class="py-3 px-6 border-b border-gray-300">
-                                        <ion-icon name="attach" size="large"></ion-icon>
-                                    </div>
-                                    <div class="p-6">
-                                        <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
-                                        <p class="text-gray-700 text-base mb-6">
-                                            With supporting text below as a natural lead-in to additional content.
-                                        </p>
+                                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+                                    <div class="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+                                        <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-green-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-blue-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="block shadow-lg bg-blue-300 max-w-sm text-center m-12">
-                                    <div class="py-3 px-6 border-b border-gray-300">
-                                        <ion-icon name="attach" size="large"></ion-icon>
-                                    </div>
-                                    <div class="p-6">
-                                        <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
-                                        <p class="text-gray-700 text-base mb-6">
-                                            With supporting text below as a natural lead-in to additional content.
-                                        </p>
+                                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+                                    <div class="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+                                        <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-green-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-blue-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
+                                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
+                                    <div class="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+                                        <div class="block shadow-lg bg-yellow-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-green-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="block shadow-lg bg-blue-300 max-w-sm text-center m-12">
+                                            <div class="py-3 px-6 border-b border-gray-300">
+                                                <ion-icon name="attach" size="large"></ion-icon>
+                                            </div>
+                                            <div class="p-6">
+                                                <h5 class="text-gray-900 text-xl font-medium mb-2">Special title treatment</h5>
+                                                <p class="text-gray-700 text-base mb-6">
+                                                    With supporting text below as a natural lead-in to additional content.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div data-dial-init class="fixed right-6 bottom-6 group">
+                                <button type="button"  class="flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+                                    <svg aria-hidden="true" class="w-8 h-8 transition-transform group-hover:rotate-45" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                                    </svg>
+                                    <span class="sr-only">Open actions menu</span>
+                                </button>
                             </div>
                         </div>
                     </div>
