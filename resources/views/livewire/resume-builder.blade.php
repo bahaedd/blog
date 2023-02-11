48a28418428f727da4fc070955da92d45c166cc7
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-11 20:51:52 +0100

    Multinotes--Setup005--

--- resources/views/livewire/resume-builder.blade.php
+++ resources/views/livewire/resume-builder.blade.php
@@ -5,7 +5,7 @@
                 <label for="tabs" class="sr-only">Select tab</label>
                 <select id="tabs" class="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option>Informations</option>
-                    <option>Preview</option>
+                    <option id="preview-tab" data-tabs-target="#preview" type="button" role="tab" aria-controls="preview-tab">Preview</option>
                 </select>
             </div>
             <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
