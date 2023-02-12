d4f06007f5d5c60f0e7bbc9fa22296e33eb179e6
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-12 20:10:08 +0100

    Multinotes--Setup007--

--- app/Http/Livewire/NoteList.php
+++ app/Http/Livewire/NoteList.php
@@ -3,11 +3,85 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Validator;
+use Carbon\Carbon;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use App\Models\Note;
+
 
 class NoteList extends Component
 {
+
+    public $personal_notes;
+    public $work_notes;
+    public $other_notes;
+    public $state = [];
+
+
     public function render()
     {
         return view('livewire.note-list');
+    }
+
+    public function mount()
+    {
+        $this->personal_notes = Note::where('category', '=', 'personal')->get();
+        $this->work_notes = Note::where('category', '=', 'work')->get();
+        $this->other_notes = Note::where('category', '=', 'others')->get();
+
+    }
+
+    public function store()
+    {
+        $validator = Validator::make($this->state, [
+            'title' => 'required',
+            'category' => 'required',
+            'description' => 'required',
+        ])->validate();
+
+        if($this->state['category'] == 'personal'){
+            Note::create([
+            'title' => $this->state['title'],
+            'category' => $this->state['category'],
+            'description' => $this->state['description'],
+        ]);
+        }
+
+        elseif($this->state['category'] == 'work'){
+            Notet::create([
+            'title' => $this->state['title'],
+            'category' => $this->state['category'],
+            'description' => $this->state['description'],
+        ]);
+        }
+
+        else{
+            Note::create([
+            'title' => $this->state['title'],
+            'category' => $this->state['category'],
+            'description' => $this->state['description'],
+        ]);
+        }
+
+
+        $this->reset('state');
+        $this->mount();
+        $this->alert('success', 'Note Added', [
+            'position' => 'center',
+            'toast' => true
+        ]);
+
+    }
+
+    public function delete($id)
+    {
+        if($id){
+            Note::where('id',$id)->delete();
+            $this->mount();
+            $this->alert('success', 'Note Deleted', [
+            'position' => 'center',
+            'toast' => true
+             ]);
+        }
     }
 }
--- app/Models/Note.php
+++ app/Models/Note.php
@@ -12,6 +12,7 @@ class Note extends Model
     protected $fillable = [
         'user_id',
         'title',
+        'category',
         'description',
     ];
 }
