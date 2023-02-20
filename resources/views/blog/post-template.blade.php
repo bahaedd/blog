325bb0367b7fd1b1f69dfc409e5f71861b4fd208
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-20 18:03:15 +0100

    template--post06--

--- resources/views/blog/post-template.blade.php
+++ resources/views/blog/post-template.blade.php
@@ -14,79 +14,79 @@
             </p>
         </div>
         <!--Post Content-->
-        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">Artisan is Laravel's command-line interface, and it comes with a range of built-in commands that can help you with common tasks like generating controllers, models, and migrations. But sometimes you may need to create a custom Artisan command to automate a task specific to your project. In this post, we'll show you how to create a custom Artisan command in Laravel .</p>
-        <h6 class="pt-6 font-body leading-relaxed text-blue-400">Step 1 : Create the Command</p>
-        <p class="pt-6 pb-6 font-body leading-relaxed text-grey-20 dark:text-white">To create a new custom Artisan command, you can use the  <code>make:command</code> Artisan command. This command will generate a new command class in the <code>app/Console/Commands</code> directory. </p>
-        <p class="pt-6 pb-6 font-body leading-relaxed text-grey-20 dark:text-white">in this tutorial I will provide an example command that will create a new file in the <code>storage/app</code> directory with the given name and content:</p>
-        <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-white  font-bold subpixel-antialiased bg-gray-900  pb-6 pt-4 rounded-lg leading-normal overflow-hidden">
-            <div class="mt-4 flex text-sm"><span class="text-green-400 mr-3">laravel-tutorials:~$</span> php artisan make:command CreateFile</div>
-        </div>
-        <p class="pt-6 pb-6 font-body leading-relaxed text-grey-20 dark:text-white">This command will generate a CreateFile class in the <code>app/Console/Commands</code>  directory. Open the <code>CreateFile.php</code> file and add the code below:</p>       
-         <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-green-400 text-sm bg-gray-900  pb-6 pt-4 rounded-lg leading-normal overflow-hidden mt-12">
+        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">When building complex database transactions in Laravel, it's important to use transactions to ensure data consistency and prevent data corruption. In this post, we'll cover the basics of using transactions in Laravel to manage database operations.</p>
+        <h3 class="pt-6 font-body leading-relaxed text-blue-700">- What are Transactions?</h3>
+        <p class="pt-6 pb-6 font-body leading-relaxed text-grey-20 dark:text-white">A database transaction is a set of database operations that are performed as a single, atomic unit of work. Transactions ensure that all the database operations in the unit of work are completed successfully, or that none of them are completed at all. If an error occurs during any of the operations, the entire transaction is rolled back, and the database is left in its original state.</p>
+        <p class="pt-6 pb-6 font-body leading-relaxed text-grey-20 dark:text-white">Transactions are critical for maintaining data consistency, especially when you have multiple database operations that depend on one another. If one operation fails, it can leave the database in an inconsistent state, which can cause serious problems for your application.</p>
+        <h3 class="pt-6 font-body leading-relaxed text-blue-700">- Use Transactions in Laravel</h3>
+        <p class="pt-6 pb-6 font-body leading-relaxed text-grey-20 dark:text-white">Laravel provides a simple way to use transactions using the DB facade. Here's an example:</p>       
+         <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-green-400 text-sm bg-gray-700  pb-6 pt-4 rounded-lg leading-normal overflow-hidden mt-12">
                 <pre class="">
-                    <p>namespace App\Console\Commands;</p>
-                    <p></p>
-                    <p>use Illuminate\Console\Command;</p>
-                    <p>use Illuminate\Support\Facades\File;</p>
-                    <p></p>
-                    <p class="text-red-400">class CreateFile extends Command</p>
+                    <p>use Illuminate\Http\Request;</p>
+                    <p>use Illuminate\Support\Facades\DB;</p>
+                    <p>use Exception;</p>
+                    <p>use App\Models\User;</p>
+                    <p>  </p>
+                    <p class="text-red-400">class UserController extends Controller</p>
                     <p class="text-red-400">{</p>
-                    <p>    /**</p>
-                    <p>     * The name and signature of the console command.</p>
-                    <p>     *</p>
-                    <p>     * @var string</p>
-                    <p>     */</p>
-                    <p class="text-blue-400">    protected $signature = 'file:create {name} {content}';</p>
-                    <p></p>
-                    <p>    /**</p>
-                    <p>     * The console command description.</p>
-                    <p>     *</p>
-                    <p>     * @var string</p>
-                    <p>     */</p>
-                    <p class="text-blue-400">    protected $description = 'Create a new file with the given name and content';</p>
-                    <p></p>
-                    <p>    /**</p>
-                    <p>     * Execute the console command.</p>
-                    <p>     *</p>
-                    <p>     * @return void</p>
-                    <p>     */</p>
-                    <p class="text-blue-400">    public function handle()</p>
+                    <p class="text-blue-400">    public function store(Request $request)</p>
                     <p class="text-blue-400">    {</p>
-                    <p class="text-blue-400">        $name = $this->argument('name');</p>
-                    <p class="text-blue-400">        $content = $this->argument('content');</p>
-                    <p></p>
-                    <p class="text-blue-400">        File::put(storage_path('app/' . $name), $content);</p>
-                    <p></p>
-                    <p class="text-blue-400">        $this->info("File {$name} created successfully!");</p>
+                    <p>        try {</p>
+                    <p>            </p>
+                    <p>            // Start DB Transaction</p>
+                    <p>            DB::beginTransaction();</p>
+                    <p>  </p>
+                    <p>            // Create New User</p>
+                    <p>            $user = User::create([</p>
+                    <p>                    'name' => $request->name,</p>
+                    <p>                    'email' => $request->email,</p>
+                    <p>                    'password' => bcrypt($request->password)</p>
+                    <p>                ]);</p>
+                    <p>  </p>
+                    <p>            // Commit Transaction to Save Data to Database</p>
+                    <p>            DB::commit();</p>
+                    <p>        } catch (Exception $e) {</p>
+                    <p>            // Rollback Database Entry</p>
+                    <p>            DB::rollback();</p>
+                    <p>            throw $e;</p>
+                    <p>        }</p>
+                    <p>  </p>
                     <p class="text-blue-400">    }</p>
                     <p class="text-red-400">}</p>
-                    <p></p>
                 </pre>  
         </div>
 
-        <h6 class="pt-6 font-body leading-relaxed text-blue-400">Step 2 :  Register the Command</p>
-        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">Next, you need to register your custom Artisan command with Laravel. Open the <code>app/Console/Kernel.php</code> file and add your command class to the $commands array:</p>
-        <!-- code source -->
-        <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-blue-400  text-sm bg-gray-900  pb-6 pt-4 rounded-lg leading-normal overflow-hidden mt-12">
-                <pre class="">
-                    <p></p>
-                    <p>protected $commands = [</p>
-                    <p>    \App\Console\Commands\CreateFile::class,</p>
-                    <p>];</p>
-                </pre>  
-        </div>
-
-        <h6 class="pt-6 font-body leading-relaxed text-blue-400">Step 3 :  Use the Command</p>
-        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">Now you can run your custom Artisan command using the <code>php artisan</code>  command. Here's an example of how to create a new file called example.txt with the content "Congrats, your Artisan custom command created successfly !":</p>
         
-        <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-white  font-bold subpixel-antialiased bg-gray-900  pb-6 pt-4 rounded-lg leading-normal overflow-hidden">
-            <div class="mt-4 flex text-sm"><span class="text-green-400 mr-3">laravel-tutorials:~$</span> php artisan file:create example.txt "Congrats, your Artisan custom command created successfly !"
-            </div>
+        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">This code begins a new database transaction using the <code>beginTransaction()</code> method. The try block contains the database operations you want to perform within the transaction. If any of the operations fail, an exception will be thrown, and the catch block will be executed. The <code>rollback()</code> method is used to undo all the operations that were performed in the transaction, so the database is left in its original state. If all the operations complete successfully, the <code>commit()</code> method is called, and the changes are saved to the database.</p>
+        
+        <h3 class="pt-6 font-body leading-relaxed text-blue-700">Nested Transactions</h3>
+        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">Laravel also supports nested transactions, which can be useful when you need to perform multiple transactions within a single database operation:</p>
+
+        <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-green-400 text-sm bg-gray-700  pb-6 pt-4 rounded-lg leading-normal overflow-hidden mt-12">
+                <pre class="">
+                    <p>DB::beginTransaction();</p>
+                    <p></p>
+                    <p>try {</p>
+                    <p>    // Perform database operations here...</p>
+                    <p>    DB::beginTransaction();</p>
+                    <p></p>
+                    <p>    try {</p>
+                    <p>        // Perform additional database operations here...</p>
+                    <p>        DB::commit();</p>
+                    <p>    } catch (\Exception $e) {</p>
+                    <p>        DB::rollback();</p>
+                    <p>    }</p>
+                    <p>    DB::commit();</p>
+                    <p>} catch (\Exception $e) {</p>
+                    <p>    DB::rollback();</p>
+                    <p>}</p>
+                </pre>  
         </div>
+        
 
-        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">This will create a new file in the <code>storage/app</code> directory with the given name and content, and output a success message to the console.</p>
+        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">In this example, we begin a new transaction using <code>beginTransaction()</code> and perform some database operations. Then we begin another transaction, perform additional operations, and commit that transaction. Finally, we commit the outer transaction. If any of the database operations fail, the transactions will be rolled back, and the database will be left in its original state..</p>
 
-        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">Creating a custom Artisan command in Laravel  is a powerful way to automate tasks specific to your project. By following the steps outlined in this post, you can create your own custom commands to improve your workflow and save time.</p>
+        <p class="pt-6 font-body leading-relaxed text-grey-20 dark:text-white">Using transactions is essential when performing complex database operations in Laravel. They ensure that your database remains in a consistent state, even in the face of errors or other problems. By using the <code class="text-red-700">DB</code> facade and the <code class="text-blue-700"> beginTransaction(), commit(), and rollback() </code> methods, you can easily manage database transactions in Laravel.</p>
 
         <p class="pt-6 pb-6 font-body leading-relaxed text-grey-20 dark:text-white">Thanks for following up to this point, I hope that was clear and helpful. Enjoy your code journey...</p>
     </div>
