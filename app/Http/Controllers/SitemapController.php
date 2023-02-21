e7607750e1e88876d356960ae3c8c5f5e47a2c50
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-21 19:23:52 +0100

    SiteMap--Update--

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -7,7 +7,11 @@ use App\Http\Controllers\Controller;
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Tool;
 use Illuminate\Http\Request;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 
 class SitemapController extends Controller
@@ -15,11 +19,13 @@ class SitemapController extends Controller
 
     
     public function index(Request $r)
-    {
-       
+    {      
         $posts = Post::orderBy('id','desc')->where('status', 'PUBLISHED')->get();
+        $categories = Category::all();
+        $tags = Tag::all();
+        $tools = Tool::all();
 
-        return response()->view('blog.sitemap', compact('posts'))
+        return response()->view('blog.sitemap', compact('posts', 'categories', 'tags', 'tools'))
           ->header('Content-Type', 'text/xml');
 
     }
--- resources/views/blog/sitemap.blade.php
+++ resources/views/blog/sitemap.blade.php
@@ -1,11 +1,54 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{url('/')}}</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>{{url('/blog')}}</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>{{url('/contact')}}</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    @foreach ($categories as $category)
+        <url>
+            <loc>{{url($category->slug)}}</loc>
+            <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($category->created_at)) }}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+        </url>
+    @endforeach
     @foreach ($posts as $post)
         <url>
             <loc>{{url($post->slug)}}</loc>
             <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($post->updated_at)) }}</lastmod>
             <changefreq>daily</changefreq>
-            <priority>0.6</priority>
+            <priority>1.0</priority>
         </url>
     @endforeach
+    @foreach ($tags as $tag)
+        <url>
+            <loc>{{url($tag->slug)}}</loc>
+            <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($tag->created_at)) }}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+        </url>
+    @endforeach
+    @foreach ($tools as $tool)
+        <url>
+            <loc>{{url($tool->url)}}</loc>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+        </url>
+    @endforeach
+    <url>
+        <loc>{{url('/about')}}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.9</priority>
+    </url>
 </urlset>
