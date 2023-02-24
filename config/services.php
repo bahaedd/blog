c0d975545c88e179542756fc13213f69503f88cf
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-24 19:44:14 +0100

    -sociallogin003-

--- config/services.php
+++ config/services.php
@@ -37,4 +37,20 @@ return [
         'key' => env('LOCATION_KEY'),
     ],
 
+    'google' => [
+   'client_id' => env('GOOGLE_CLIENT_ID'),
+   'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+   'redirect' => 'your_redirect_url',
+    ],
+    'facebook' => [
+       'client_id' => env('FACEBOOK_CLIENT_ID'),
+       'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+       'redirect' => 'your_redirect_url',
+    ],
+    'github' => [
+       'client_id' => env('GITHUB_CLIENT_ID'),
+       'client_secret' => env('GITHUB_CLIENT_SECRET'),
+       'redirect' => 'your_redirect_url',
+    ],
+
 ];
