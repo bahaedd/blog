c11f7438e9a1ab5275a5c5dbb34e188f59171abc
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-03-03 12:46:20 +0100

    CompileAssets

--- resources/views/livewire/note-list.blade.php
+++ resources/views/livewire/note-list.blade.php
@@ -29,7 +29,7 @@
                 </div>
                 <div class="flex justify-items-center mt-12 ml-3">
                     <a href="{{ route('personalpack') }}" class="text-green-700 mt-4 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
-                        <ion-icon name="chevron-back-outline"></ion-icon> Back to PersonalPack
+                        <ion-icon name="chevron-back-outline"></ion-icon> PersonalPack
                     </a>
                 </div>
             </div>
