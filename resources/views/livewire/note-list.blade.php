f0657d62cc56f11efdf6d90ff79408621f0a9732
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-03-06 22:25:01 +0100

    refactoring2

--- resources/views/livewire/note-list.blade.php
+++ resources/views/livewire/note-list.blade.php
@@ -83,7 +83,7 @@
                                 <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
                                 </svg>
-                                Add new product
+                                Add new note
                             </button>
                         </form>
                     </div>
@@ -112,7 +112,7 @@
                             </div>
                         </div>
                         @empty
-                            <p class="text-center font-normal text-gray-500 mt-6">No notes to show yet...</p>
+                            <p class="text-center font-normal text-gray-500 mt-6">Personal Notes</p>
                             @endforelse
                     </div>
                 </div>
@@ -140,7 +140,7 @@
                             </div>
                         </div>
                          @empty
-                            <p class="text-center font-normal text-gray-500 mt-6">No notes to show yet...</p>
+                            <p class="text-center font-normal text-gray-500 mt-6">Work notes</p>
                             @endforelse
                     </div>
                 </div>
@@ -168,7 +168,7 @@
                             </div>
                         </div>
                         @empty
-                            <p class="text-center font-normal text-gray-500 mt-6">No notes to show yet...</p>
+                            <p class="text-center font-normal text-gray-500 mt-6">Other Notes</p>
                             @endforelse
                     </div>
                 </div>
