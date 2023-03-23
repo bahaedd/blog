c06b7d8ec16302ae8a9385047acee8773aeb91b0
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-03-23 17:54:27 +0000

    refactoring

--- resources/views/blog/tools/adhan.blade.php
+++ resources/views/blog/tools/adhan.blade.php
@@ -9,6 +9,14 @@
     <!-- container -->
     <div class="px-16 mx-auto py-16 md:py-20 mb-6">
         <h2 class="my-4 mb-3 text-4xl text-center font-semibold text-yellow-300 rounded md:bg-transparent md:p-0 dark:text-yellow-300">Adhan Times</h2>
+        <label for="underline_select" class="sr-only">Underline select</label>
+         <select id="underline_select" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+             <option selected>Choose a country</option>
+             <option value="US">United States</option>
+             <option value="CA">Canada</option>
+             <option value="FR">France</option>
+             <option value="DE">Germany</option>
+         </select>
     </div>
     <div>
         <div class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto mx-12">
