702bd5ebb137ef7e0469e7b4ee80b91073ea84de
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-03-24 18:00:46 +0000

    refactoring3

--- resources/views/blog/tools/email-extractor.blade.php
+++ resources/views/blog/tools/email-extractor.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    @section('title', "Email Address Extractor")
     @include('/blog/layouts.head')
 
     <body class="border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800">
-        <!-- navbar -->
+        
         @include('/blog/layouts.navbar')
 
-        <!-- container -->
+        
         <div class="px-16 mx-auto py-16 md:py-20 mb-24">
             <h2 class="my-4 mb-12 text-4xl text-center font-semibold text-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-green-700">Email Address Extractor</h2>
             <form class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0" method="POST" action="{{url('/projects/emailextractor/email-extract')}}">
@@ -47,7 +46,7 @@
             </div>
         </div>
 
-        <!-- Footer -->
+        
         @include('/blog/layouts.footer')
         <livewire:scripts />
         <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.8/dist/clipboard.min.js"></script>
@@ -94,7 +93,7 @@
         </script>
         <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
         <script>
-        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
         } else {
@@ -105,7 +104,7 @@
         var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
         var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-        // Change the icons inside the button based on previous settings
+        
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             themeToggleLightIcon.classList.remove('hidden');
         } else {
@@ -116,11 +115,11 @@
 
         themeToggleBtn.addEventListener('click', function() {
 
-        // toggle icons inside button
+        
         themeToggleDarkIcon.classList.toggle('hidden');
         themeToggleLightIcon.classList.toggle('hidden');
 
-        // if set via local storage previously
+        
         if (localStorage.getItem('color-theme')) {
             if (localStorage.getItem('color-theme') === 'light') {
                 document.documentElement.classList.add('dark');
@@ -130,7 +129,7 @@
                 localStorage.setItem('color-theme', 'light');
             }
 
-        // if NOT set via local storage previously
+        
         } else {
             if (document.documentElement.classList.contains('dark')) {
                 document.documentElement.classList.remove('dark');
--- resources/views/blog/tools/iplocation.blade.php
+++ resources/views/blog/tools/iplocation.blade.php
@@ -1,111 +1,51 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-@section('title', "PersonalPack")
-@include('/blog/layouts.head')
+@extends('/blog/main')
+@section('content')
+<section class="bg-white dark:bg-gray-900">
+    <div class="flex justify-center min-h-screen">
+        <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+            <div class="w-full">
+                <h1 class="text-2xl text-center font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
+                    Get in touch.
+                </h1>
 
-<body class="border-b border-gray-200 dark:border-gray-600 dark:bg-gray-900">
-   
-    @include('/blog/layouts.navbar')
-    <div class="container mx-auto py-3 mt-12 dark:bg-gray-900">
-        <div class="mt-12 mb-24">
-            <section class="bg whitedark:bg-gray-800">
-                <div class="max-w-6xl mx-auto px-5 py-24 ">
-                    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center mb-12">
-                        <h2 class="my-4 text-3xl text-center font-semibold text-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-green-700">PersonalPack</h2>
-                    </div>
-                    <div class="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
-                        @foreach ($tools as $tool)
-                        <div class="group rounded px-8 py-12 shadow bg-blue-600 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
-                            <a href="{{ route($tool->url) }}">
-                                <div class="mx-auto h-8 w-8 text-center">
-                                    {!! $tool->icon !!}
-                                </div>
-                                <div class="text-center">
-                                    <h3 class="pt-2 text-lg font-semibold text-primary group-hover:text-yellow lg:text-xl">{{ $tool->name }}</h3>
-                                </div>
-                            </a>
+                <p class="mt-4 mb-6 text-center text-gray-500 dark:text-gray-400">
+                    Got a technical issue? Need help? Wanna hire me? Let me know.
+                </p>
+                <form class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md" action="{{url('portfolio/store')}}" method="post">
+                    @csrf
+                    @if (session()->has('success'))
+                        <div class="flex p-4 mb-4 text-sm text-center text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                          <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                          <span class="sr-only">Info</span>
+                          <div>
+                            <span class="font-medium">{{ session()->get('success') }}</span>
+                          </div>
                         </div>
-                        @endforeach
+                    @endif
+                    <div class="flex flex-col md:flex-row">
+                        <input class="mr-3 w-full bg-gray-50 rounded border-grey-50 px-4 py-3 font-body  md:w-1/2 lg:mr-5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500  py-4 px-12  dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 {{ $errors->has('name') ? 'error' : '' }}" placeholder="Name" type="text" name="name" id="name">
+                        <!-- Error -->
+                        @if ($errors->has('name'))
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Please enter your name.</p>
+                        @endif
+                        <input class="mt-6 w-full bg-gray-50 rounded border-grey-50 px-4 py-3 font-body text-black md:mt-0 md:ml-3 md:w-1/2 lg:ml-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500  py-4 px-12  dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 {{ $errors->has('email') ? 'error' : '' }}" placeholder="Email" type="text" name="email" id="email">
+                        @if ($errors->has('email'))
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Please enter your email.</p>
+                        @endif
                     </div>
-                </div>
-            </section>
+                    <textarea class="mt-6 w-full bg-gray-50 rounded border-grey-50 px-4 py-3 font-body text-black md:mt-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500  py-4 px-12  dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 {{ $errors->has('message') ? 'error' : '' }}" placeholder="Message" id="message" cols="30" rows="10" name="message"></textarea>
+                    @if ($errors->has('message'))
+                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Please enter your message.</p>
+                    @endif
+                    <div class="text-center mt-4">
+                    <button type="submit" name="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      SUBMIT
+                      <ion-icon name="send-outline" class="w-5 h-5 ml-2 -mr-1"></ion-icon>
+                    </button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-    @include('/blog/layouts.footer')
-    <livewire:scripts />
-    <script>
-    function getCarouselData() {
-        return {
-            currentIndex: 0,
-            images: [
-                'https://source.unsplash.com/collection/1346951/800x800?sig=1',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=2',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=3',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=4',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=5',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=6',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=7',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=8',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=9',
-            ],
-            increment() {
-                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex + 1;
-            },
-            decrement() {
-                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex - 1;
-            },
-        }
-    }
-
-    </script>
-    <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
-    <script>
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
-
-    </script>
-    <script>
-    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        themeToggleLightIcon.classList.remove('hidden');
-    } else {
-        themeToggleDarkIcon.classList.remove('hidden');
-    }
-
-    var themeToggleBtn = document.getElementById('theme-toggle');
-
-    themeToggleBtn.addEventListener('click', function() {
-
-        themeToggleDarkIcon.classList.toggle('hidden');
-        themeToggleLightIcon.classList.toggle('hidden');
-
-        if (localStorage.getItem('color-theme')) {
-            if (localStorage.getItem('color-theme') === 'light') {
-                document.documentElement.classList.add('dark');
-                localStorage.setItem('color-theme', 'dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-                localStorage.setItem('color-theme', 'light');
-            }
-
-        } else {
-            if (document.documentElement.classList.contains('dark')) {
-                document.documentElement.classList.remove('dark');
-                localStorage.setItem('color-theme', 'light');
-            } else {
-                document.documentElement.classList.add('dark');
-                localStorage.setItem('color-theme', 'dark');
-            }
-        }
-
-    });
-
-    </script>
-</body>
-
-</html>
+</section>
+@endsection
