f1548f45c444a3d0076dfc4e6a32eeee4fd1e0c2
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-03-26 18:17:26 +0000

    AsmaaAlhosna2

--- resources/views/blog/tools/asmaa.blade.php
+++ resources/views/blog/tools/asmaa.blade.php
@@ -6,8 +6,7 @@
     @include('/blog/layouts.navbar')
     <div class="px-16 mx-auto py-16 md:py-20 mb-6 mt-24">
         <ul class="grid w-full gap-6 md:grid-cols-5">
-            @for ($i = 0; $i < 99; $i++)
-            <li>
+            @for ($i = 0; $i < 99; $i++) <li>
                 <input type="radio" id="hosting-small" name="hosting" value="hosting-small" class="hidden peer" required>
                 <label for="hosting-small" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                     <div class="block">
@@ -16,8 +15,8 @@
                         <div class="w-full">{{ $data['data'][$i]['en']['meaning'] }}</div>
                     </div>
                 </label>
-            </li>
-            @endfor
+                </li>
+                @endfor
         </ul>
     </div>
     <livewire:scripts />
@@ -48,7 +47,7 @@
     </script>
     <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
     <script>
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark');
     } else {
@@ -71,11 +70,11 @@
 
     themeToggleBtn.addEventListener('click', function() {
 
-        // toggle icons inside button
+        
         themeToggleDarkIcon.classList.toggle('hidden');
         themeToggleLightIcon.classList.toggle('hidden');
 
-        // if set via local storage previously
+        
         if (localStorage.getItem('color-theme')) {
             if (localStorage.getItem('color-theme') === 'light') {
                 document.documentElement.classList.add('dark');
@@ -85,7 +84,7 @@
                 localStorage.setItem('color-theme', 'light');
             }
 
-            // if NOT set via local storage previously
+            
         } else {
             if (document.documentElement.classList.contains('dark')) {
                 document.documentElement.classList.remove('dark');
