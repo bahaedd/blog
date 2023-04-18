039982505dac1d0206c83947447df24535da6ae9
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-04-18 17:38:28 +0000

    EmailValidator7

---
+++ resources/views/blog/toolsv2/multiple-email-validator.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@include('/blog/layouts.head')
+
+<body class="border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800">
+    @include('/blog/layouts.navbar')
+    <div class="px-16 mx-auto py-16 md:py-20 mb-72">
+        <h2 class="my-4 mb-12 text-4xl text-center font-semibold text-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-blue-700">Email Address Validator</h2>
+        <nav class="flex mb-12 mt-12 ml-6" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <li class="inline-flex items-center">
+                    <a href="" class="inline-flex items-center text-sm font-medium text-blue-700 hover:text-blue-600 dark:text-blue-400 dark:hover:text-white">
+                        <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                        </svg>
+                        Tools
+                    </a>
+                </li>
+                <li>
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                        </svg>
+                        <a href="{{ route('mailerpack') }}" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">MailerPack</a>
+                    </div>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Email Address Validator</span>
+                    </div>
+                </li>
+            </ol>
+        </nav>
+        <form class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0" action="{{url('projects/email-validator/check')}}" method="post">
+            @csrf
+            <section class="w-full md:w-2/4 flex flex-col px-4 m-b-3 md:px-6 text-xl text-white-800 leading-normal">
+                <div class="relative mt-12">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                    <input type="search" id="email" name="email" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="valid@example.com" required>
+                    <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Check</button>
+                </div>
+                @if ($errors->any())
+                <div class="flex p-4 mt-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                    <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="sr-only">Info</span>
+                    <div>
+                        @foreach ($errors->all() as $error)
+                        <span class="font-medium">{{ $error }}</span>
+                        @endforeach
+                    </div>
+                </div>
+                @endif
+            </section>
+            <section class="w-full md:w-2/4 flex flex-col items-center px-4 m-b-3 md:px-6 text-xl text-white-800 leading-normal mt-6 {{ $hidden }}">
+                <h4 class="mb-4 text-center text-md font-semibold text-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-green-700">{{ session()->get('domain') }}</h4>
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Address
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Status
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Free Email
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    SMTP Provider
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    MX Record
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $data['address'] }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    @if($data['status'] = 'valid')
+                                    <span class="font-medium">
+                                        <div class="flex items-center">
+                                            <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div> Valid
+                                        </div>
+                                    </span>
+                                    @else
+                                    <span class="font-medium">
+                                        <div class="flex items-center">
+                                            <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> Invalid
+                                        </div>
+                                    </span>
+                                    @endif
+                                </td>
+                                <td class="px-6 py-4">
+                                    @if(empty($data['free_email']))
+                                    <span class="font-medium">
+                                        No
+                                    </span>
+                                    @else
+                                    <span class="font-medium">
+                                        Yes
+                                    </span>
+                                    @endif
+                                </td>
+                                <td class="px-6 py-4">
+                                    @if(empty($data['smtp_provider']))
+                                    <span class="font-medium">
+                                        Not found
+                                    </span>
+                                    @else
+                                    <span class="font-medium">
+                                        {{ $data['smtp_provider'] }}
+                                    </span>
+                                    @endif
+                                </td>
+                                <td class="px-6 py-4">
+                                    @if($data['mx_found'] = 'true')
+                                    <span class="font-medium">
+                                        {{ $data['mx_record'] }}
+                                    </span>
+                                    @else
+                                    <span class="font-medium">
+                                        Not found
+                                    </span>
+                                    @endif
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </form>
+    </div>
+    <script defer src="https://unpkg.com/alpinejs@3.2.4/dist/cdn.min.js"></script>
+    @include('/blog/layouts.footer')
+    <livewire:scripts />
+    <script>
+    function getCarouselData() {
+        return {
+            currentIndex: 0,
+            images: [
+                'https://source.unsplash.com/collection/1346951/800x800?sig=1',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=2',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=3',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=4',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=5',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=6',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=7',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=8',
+                'https://source.unsplash.com/collection/1346951/800x800?sig=9',
+            ],
+            increment() {
+                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex + 1;
+            },
+            decrement() {
+                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex - 1;
+            },
+        }
+    }
+
+    </script>
+    <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+    <script>
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark')
+    }
+
+    </script>
+    <script>
+    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        themeToggleLightIcon.classList.remove('hidden');
+    } else {
+        themeToggleDarkIcon.classList.remove('hidden');
+    }
+    var themeToggleBtn = document.getElementById('theme-toggle');
+    themeToggleBtn.addEventListener('click', function() {
+
+        themeToggleDarkIcon.classList.toggle('hidden');
+        themeToggleLightIcon.classList.toggle('hidden');
+
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
+
+        } else {
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
+        }
+    });
+
+    </script>
+</body>
+
+</html>
