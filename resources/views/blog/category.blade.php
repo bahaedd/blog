3287105cb2263edec4b7ea8490e333b0794834b7
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-05-06 15:32:13 +0100

    category-page-update2

--- resources/views/blog/category.blade.php
+++ resources/views/blog/category.blade.php
@@ -1,42 +1,29 @@
 @extends('/blog/main')
 @section('content')
- <!-- container -->
-<div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-color: black; height: 60vh; max-height:460px;">
-    <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
-        <!--Title-->
-        <p class="text-green-500 font-extrabold text-3xl md:text-5xl">
-            {{ $m_category?->name }}
-        </p>
-        <p class="text-xl md:text-2xl text-gray-500 mt-6">{{ $m_category?->description }}</p>
+<div class="container mx-auto py-3 mt-12 mb-12 dark:bg-gray-900">
+    <h2 class="my-4 text-xl text-center font-bold text-white rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-gray-400 uppercase">{{ $m_category?->name }}</h2>
+    <div class="w-full mb-12">
+        <div class="h-1 mx-auto gradient w-64 my-0 py-0 rounded-t bg-green-700"></div>
     </div>
-</div>
-<!--Container-->
-<div class="container px-4 md:px-0 max-w-12xl mx-auto -mt-32">
-    <div class="mx-0 sm:mx-6">
-        <div class="text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-gray-400 w-full text-xl md:text-2xl leading-normal rounded-t">
-            <!--Posts Container-->
-            <div class="flex flex-wrap justify-between p-12 -mx-6">
-                @foreach ($posts as $post)
-                <div class="w-full md:w-1/4 p-6 flex flex-col flex-grow flex-shrink">
-                    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700 mx-3">
-                                    <a href="/blog/post/{{ $post->slug }}" title="{{ $post->title }}">
-                                        <img class="rounded-t-lg" src="{{Voyager::image( $post->image )}}" alt="{{ $post->title }}" width="100%" height="100%" />
-                                    </a>
-                                    <div class="p-5">
-                                        <a href="/blog/post/{{ $post->slug }}" title="{{ $post->title }}">
-                                            <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h3>
-                                        </a>
-                                        <p class="mb-3 text-gray-700 text-sm font-semibold dark:text-gray-400">{{ Str::substr($post->excerpt, 0, 120)}}...</p>
-                                    </div>
-                                </div>
+    <div class="flex flex-wrap mt-12">
+        @foreach ($posts as $post)
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 mb-4 dark:bg-gray-900 mt-4 mb-6 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
+            <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700 mx-3">
+                <a href="/blog/post/{{ $post->slug }}" title="{{ $post->title }}">
+                    <img class="rounded-t-lg" src="{{Voyager::image( $post->image )}}" alt="{{ $post->title }}" width="100%" height="100%" />
+                </a>
+                <div class="p-5">
+                    <a href="/blog/post/{{ $post->slug }}" title="{{ $post->title }}">
+                        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h3>
+                    </a>
+                    <p class="mb-3 text-gray-700 text-sm font-semibold dark:text-gray-400">{{ Str::substr($post->excerpt, 0, 120)}}...</p>
                 </div>
-                @endforeach
             </div>
-            <!--/ Post Content-->
-            <div class="p-3 mt-6">
-        {{ $posts->links('pagination::simple-tailwind') }}
-    </div>
         </div>
+        @endforeach
+    </div>
+    <div class="p-3 mt-6">
+                {{ $posts->links('pagination::simple-tailwind') }}
     </div>
 </div>
 @endsection
