9ad55c575199f74797e674705d56564f7e3ca9e9
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-05-09 23:30:02 +0100

    category-page-update5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -56,6 +56,19 @@ class PostController extends Controller
         $title = '{{ $title }}';
         $content = '{{ $content }}';
 
+                seo()
+        ->title('AlienDev | Web Development tutorials')
+        ->rawTag('<meta name="keywords" content="AlienDev, Alien Dev, Laravel, Laravel Tutorial For Beginners, TailwindCSS Tutorial For Beginners, web development" />')
+        ->description('AlienDev here you can improve your programming skills')
+        ->url(url()->current())
+        ->image(URL('/images/alien.png'))
+        ->locale('en_US')
+        ->twitterCreator('Bahaedd97952415')
+        ->twitterSite('Bahaedd97952415')
+        ->twitterTitle('AlienDev | Web Development tutorials')
+        ->twitterDescription('AlienDev here you can improve your programming skills')
+        ->twitterImage(URL('/images/alien.png'));
+
         return view("blog.post-template", compact("latest_posts", "s_post", "laravel_posts", "title", "content"));
     }
 
