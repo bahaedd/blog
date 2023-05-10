c1f9fdb6a01397ecb632e9df3819d084d66c2232
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-05-10 12:33:42 +0100

    update

--- app/Http/Controllers/ArticleGenerator.php
+++ app/Http/Controllers/ArticleGenerator.php
@@ -9,6 +9,19 @@ class ArticleGenerator extends Controller
 {
     public function index(Request $input)
     {
+        seo()
+        ->title('AlienDev | Web Development tutorials')
+        ->rawTag('<meta name="keywords" content="AlienDev, Alien Dev, Laravel, Laravel Tutorial For Beginners, TailwindCSS Tutorial For Beginners, web development" />')
+        ->description('AlienDev here you can improve your programming skills')
+        ->url(url()->current())
+        ->image(URL('/images/alien.png'))
+        ->locale('en_US')
+        ->twitterCreator('Bahaedd97952415')
+        ->twitterSite('Bahaedd97952415')
+        ->twitterTitle('AlienDev | Web Development tutorials')
+        ->twitterDescription('AlienDev here you can improve your programming skills')
+        ->twitterImage(URL('/images/alien.png'));
+        
         if ($input->title == null) {
             return;
         }
